5f155548a15d40ca9de41b635093dca5a3740dbd
ouyangcm <mingorg@163.com>
2024-08-27 15:24:28 +0800

    简单测试Path的路径匹配

--- src/test/java/com/example/springcloudgatewaysimple/SpringCloudGatewaySimpleApplicationTests.java
+++ src/test/java/com/example/springcloudgatewaysimple/SpringCloudGatewaySimpleApplicationTests.java
@@ -3,7 +3,14 @@ package com.example.springcloudgatewaysimple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.server.PathContainer;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,5 +57,49 @@ public class SpringCloudGatewaySimpleApplicationTests {
                 .expectBody()
                 .consumeWith(
                         response -> assertThat(response.getResponseBody()).isEqualTo("demo".getBytes()));
+    }
+
+    public static void main(String[] args) {
+
+        testPathPatternParser(true, "/get/1/");
+
+    }
+
+    public static void testPathPatternParser(boolean matchTrailingSlash, String path){
+
+        PathPatternParser pathPatternParser = new PathPatternParser();
+        pathPatternParser.setMatchOptionalTrailingSeparator(matchTrailingSlash);
+        List<String> list = Arrays.asList("/get/1");
+
+        List<PathPattern> pathPatterns = new ArrayList<>();
+        list.forEach(pattern -> {
+            PathPattern pathPattern = pathPatternParser.parse(pattern);
+            pathPatterns.add(pathPattern);
+        });
+
+        PathContainer pathContainer = PathContainer.parsePath(path);
+
+        PathPattern match = null;
+        for (int i = 0; i < pathPatterns.size(); i++) {
+            PathPattern pathPattern = pathPatterns.get(i);
+            if (pathPattern.matches(pathContainer)) {
+                match = pathPattern;
+                break;
+            }
+        }
+        if (match != null) {
+            traceMatch("Pattern", match.getPatternString(), path, true);
+        }else {
+            traceMatch("Pattern", list, path, false);
+        }
+
+
+    }
+
+    private static void traceMatch(String prefix, Object desired, Object actual, boolean match) {
+
+        String message = String.format("%s \"%s\" %s against value \"%s\"", prefix, desired, match ? "matches" : "does not match", actual);
+        System.out.println(message);
+
     }
 }
